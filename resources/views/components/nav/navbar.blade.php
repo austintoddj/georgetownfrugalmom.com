d8dd228537f86f6c32a39cc5630a7e61cb7af3de
Todd Austin <austin.todd.j@gmail.com>
2019-02-07 11:45:43 -0600

    remove the registration link

--- resources/views/components/nav/navbar.blade.php
+++ resources/views/components/nav/navbar.blade.php
@@ -11,11 +11,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
                 @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle py-1" href="#" role="button"
